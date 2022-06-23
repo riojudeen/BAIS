605f16e3f37c16dcbbbd9813d966a35ac400b2ae
riojudeen <rio.setiawanj@gmail.com>
2022-06-23 18:23:19 +0700

    ljaljla

--- dashboard/pages/achievement.php
+++ dashboard/pages/achievement.php
@@ -17,151 +17,126 @@ if(isset($_SESSION['user'])){
     $endDate = date('Y-m-t');
     if($level >=1 && $level <=8){
         ?>
-        <style>
-            .sidebar2 {
-            /* width: 250px; */
-            position: absolute;
-            top: 0;
-            bottom: 0;
-            background-color: green;
-            }
-
-            main2 { margin-left: 250px; }
-        </style>
-       
+        
         <div class="row ">
-            <div class="col-md-12 ">
-                <div class="alert alert-info alert-with-icon alert-dismissible fade show" data-notify="container">
-                    <button type="button" aria-hidden="true" class="close" data-dismiss="alert" aria-label="Close">
-                    <i class="nc-icon nc-simple-remove"></i>
-                    </button>
-                    <span data-notify="icon" class="nc-icon nc-bell-55"></span>
-                    <span data-notify="message">Under Optimizing</span>
-                </div>
-            </div>
-            <div class="col-md-12">
-                <div class="row content">
-                    <div class="col-md-3 sidebar2">
-                        <div class="card card-user card-plain border rounded-lg sidebar2">
-                            <div class="sticker">
-                                
-                                <div class="nav-tabs-wrapper">
-                                    <ul id="tabs" class="nav nav-tabs flex-column nav-stacked text-left" role="tablist">
-                                        <!--  -->
-                                        <li class="nav-item ">
-                                            <a class="btn  btn-link btn-round btn-info org navigasi-absensi "  data-toggle="tab" data-id="mp" href="#mp" role="tab" aria-expanded="true"></i>Employee Movement</a>
-                                        </li>
-                                        
-                                        <li class="nav-item ">
-                                            <a class="btn  btn-link btn-round btn-info org navigasi-absensi"  data-toggle="tab" data-id="at" href="#at" role="tab" aria-expanded="true">Attendance Rate</a>
-                                        </li>
-                                        <li class="nav-item ">
-                                            <a class="btn btn-link btn-round btn-info org navigasi-absensi active data-active"  data-toggle="tab" data-id="ot" href="#ot" role="tab" aria-expanded="true">Overtime Achievement</a>
-                                        </li>
-                                        <li class="nav-item ">
-                                            <a class="btn btn-link btn-round btn-info org navigasi-absensi"  data-toggle="tab" data-id="sc" href="#sc" role="tab" aria-expanded="true">Request Overview</a>
-                                        </li>
-                                        
-                                    </ul>
+            <div class="col-md-3 position-sticky" >
+                <!-- <div class="card card-plain" >  -->
+                    <div class="nav-tabs-wrapper "  data-spy="affix" data-offset-top="205" >
+                        <ul id="tabs" class="nav nav-tabs flex-column nav-stacked text-left "   role="tablist">
+                            <!--  -->
+                            <li class="nav-item w-100">
+                                <a class="btn  btn-link btn-round btn-info org navigasi-absensi "  data-toggle="tab" data-id="mp" href="#mp" role="tab" aria-expanded="true">
+                                <i class="nc-icon nc-circle-10"></i>  
+                                Employee Movement</a>
+                            </li>
+                            
+                            <li class="nav-item ">
+                                <a class="btn  btn-link btn-round btn-info org navigasi-absensi"  data-toggle="tab" data-id="at" href="#at" role="tab" aria-expanded="true">Attendance Rate</a>
+                            </li>
+                            <li class="nav-item ">
+                                <a class="btn btn-link btn-round btn-info org navigasi-absensi active data-active"  data-toggle="tab" data-id="ot" href="#ot" role="tab" aria-expanded="true">Overtime Achievement</a>
+                            </li>
+                            <li class="nav-item ">
+                                <a class="btn btn-link btn-round btn-info org navigasi-absensi"  data-toggle="tab" data-id="sc" href="#sc" role="tab" aria-expanded="true">Request Overview</a>
+                            </li>
+                            
+                        </ul>
+                    </div>
+               <!-- </div> -->
+            </div> 
+            <div class="col-md-9 ">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-6">
+                            <h5 class="title title-page">Area</h5>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="input-group no-border">
+                                    <input type="text" class="form-control datepicker" id="start_date" data-date-format="YYYY-MM-DD" value="<?=$startDate?>">
+                                    <input type="text" disabled class="form-control text-center"  value="to" style="max-width:50px">
+                                    <input type="text" class="form-control datepicker" data-date-format="YYYY-MM-DD" id="end_date" value="<?=$endDate?>">
+                                    
+                                    
+                                    <div class="input-group-append ">
+                                        <span id="filterDate" class="btn btn-sm input-group-text text-sm px-2 py-0 m-0">go</span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-md-9">
-                        <div class="card">
-                            <div class="card-body">
-                                
-                                <div class="row">
-                                    <div class="col-md-6">
-                                    <h5 class="title title-page">Area</h5>
+                        <hr class="py-0 mx-0">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="input-group no-border">
+                                    <select class="form-control" name="div" id="s_div">
+                                        <option value="">Pilih Divisi</option>
+                                    </select>
+                                    <select class="form-control" name="dept" id="s_dept">
+                                        <option value="">Pilih Department</option>
+                                        <option value="" disabled>Pilih Division Terlebih Dahulu</option>
+                                    </select>
+                                    <select class="form-control" name="section" id="s_section">
+                                        <option value="">Pilih Section</option>
+                                        <option value="" disabled>Pilih Department Terlebih Dahulu</option>
+                                    </select>
+                                    <select class="form-control" name="groupfrm" id="s_goupfrm">
+                                        <option value="">Pilih Group</option>
+                                        <option value="" disabled>Pilih Section Terlebih Dahulu</option>
+                                    </select>
+                                    
+                                    <div class="input-group-append ">
+                                        <span id="filterGo" class="btn btn-sm input-group-text text-sm px-2 py-0 m-0">go</span>
                                     </div>
-                                    <div class="col-md-6">
-                                        <div class="input-group no-border">
-                                            <input type="text" class="form-control datepicker" id="start_date" data-date-format="YYYY-MM-DD" value="<?=$startDate?>">
-                                            <input type="text" disabled class="form-control text-center"  value="to" style="max-width:50px">
-                                            <input type="text" class="form-control datepicker" data-date-format="YYYY-MM-DD" id="end_date" value="<?=$endDate?>">
-                                            
-                                            
-                                            <div class="input-group-append ">
-                                                <span id="filterDate" class="btn btn-sm input-group-text text-sm px-2 py-0 m-0">go</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <hr class="py-0 mx-0">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="input-group no-border">
-                                            <select class="form-control" name="div" id="s_div">
-                                                <option value="">Pilih Divisi</option>
-                                            </select>
-                                            <select class="form-control" name="dept" id="s_dept">
-                                                <option value="">Pilih Department</option>
-                                                <option value="" disabled>Pilih Division Terlebih Dahulu</option>
-                                            </select>
-                                            <select class="form-control" name="section" id="s_section">
-                                                <option value="">Pilih Section</option>
-                                                <option value="" disabled>Pilih Department Terlebih Dahulu</option>
-                                            </select>
-                                            <select class="form-control" name="groupfrm" id="s_goupfrm">
-                                                <option value="">Pilih Group</option>
-                                                <option value="" disabled>Pilih Section Terlebih Dahulu</option>
-                                            </select>
-                                            
-                                            <div class="input-group-append ">
-                                                <span id="filterGo" class="btn btn-sm input-group-text text-sm px-2 py-0 m-0">go</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12 spinner_load " style="display:none">
-                                        <div class="card shadow-none">
-                                            <div class="card-body " style="background-image: linear-gradient(to right, rgb(255,255,255), rgb(244,243,239)  ,rgb(255,255,255));">
-                                                <div class="text-center" >
-                                                    <img id="img-spinner" src="../../assets/img/loading/load.gif" style="height:50px">
-                                                    <label class="label">please be patient , your request is being downloaded and may take a minutes...</label>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <?php
-                                    // $today = date('Y-m-d');
-
-                                    // print_r($data_tanggal);
-                                    $q_org = "SELECT `id`,`nama_org`,`cord`,`nama_cord`,`id_parent`,`part` FROM view_cord_area ";
-                                    $q_div = $q_org." WHERE id_parent = '1' AND part = 'division'";
-                                    $q_div = mysqli_query($link, $q_div )or die(mysqli_error($link));
-                                    $shift =  (isset($_GET['shift']) && $_GET['shift'] != '')?" AND absensi.shift = '$_GET[shift]' ":'';
-                                    $org_shift =  (isset($_GET['shift']) && $_GET['shift'] != '')?" AND shift = '$_GET[shift]' ":'';
-                                    // echo mysqli_num_rows($q_div);
-
-
-                                    ?>
-                                    <!-- divisi efficiency -->
-                                    <div class="col-md-12">
-                                        <div class="card card-plain">
-                                            
-                                            <div class="card-body ">
-                                                <div class="row">
-                                                    <div class="col-md-12">
-                                                        <div id="load_data"></div>
-                                                        
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-
                                 </div>
                             </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12 spinner_load " style="display:none">
+                                <div class="card shadow-none">
+                                    <div class="card-body " style="background-image: linear-gradient(to right, rgb(255,255,255), rgb(244,243,239)  ,rgb(255,255,255));">
+                                        <div class="text-center" >
+                                            <img id="img-spinner" src="../../assets/img/loading/load.gif" style="height:50px">
+                                            <label class="label">please be patient , your request is being downloaded and may take a minutes...</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <?php
+                            // $today = date('Y-m-d');
+
+                            // print_r($data_tanggal);
+                            $q_org = "SELECT `id`,`nama_org`,`cord`,`nama_cord`,`id_parent`,`part` FROM view_cord_area ";
+                            $q_div = $q_org." WHERE id_parent = '1' AND part = 'division'";
+                            $q_div = mysqli_query($link, $q_div )or die(mysqli_error($link));
+                            $shift =  (isset($_GET['shift']) && $_GET['shift'] != '')?" AND absensi.shift = '$_GET[shift]' ":'';
+                            $org_shift =  (isset($_GET['shift']) && $_GET['shift'] != '')?" AND shift = '$_GET[shift]' ":'';
+                            // echo mysqli_num_rows($q_div);
+
+
+                            ?>
+                            <!-- divisi efficiency -->
+                            <div class="col-md-12">
+                                <div class="card card-plain">
+                                    
+                                    <div class="card-body ">
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <div id="load_data"></div>
+                                                
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
                         </div>
                     </div>
                 </div>
             </div>
+                
         </div>
         <?php
         }else{
